a461e106f961c77751c3d855d217c999e2c4c660
“Melissa” <melissa@melissajclark.ca>
2014-12-03 11:52:47 -0500

    renamed maker class to creator

--- museumsAPI.js
+++ museumsAPI.js
@@ -90,7 +90,7 @@ artApp.displayPieces = function(pieces) {
 				if (artPiece.webImage !== null) {	
 					artModuleUl.append( "<li class='artMetaData'>" + "<h3><a target='_blank' href=" + artLink + ">" + "<span class='title'>"+ artTitle + "</span></a></h3></li>"); // title & link to item
 					// artModuleUl.append("<li class='artMetaData'><span class='fieldType'>Title: </span>" + artTitle +"</li>");
-					artModuleUl.append("<li class='artMetaData'><span class='fieldType'>Creator: </span><span class='maker'>" + artMaker +"</span></li>");
+					artModuleUl.append("<li class='artMetaData'><span class='fieldType'>Creator: </span><span class='creator'>" + artMaker +"</span></li>");
 				}
 
 				// let's inject the location (only if it doesn't return as undefined) into the page
