550fb537931cca3f58925b19bf57cfd9af6ea6df
“Melissa” <melissa@melissajclark.ca>
2014-12-03 11:49:35 -0500

    added span classes to li items (title and creator) - to be used for mixitup markup

--- museumsAPI.js
+++ museumsAPI.js
@@ -89,9 +89,9 @@ artApp.displayPieces = function(pieces) {
 
 				// let's inject the title & other fields into the page
 				if (artPiece.webImage !== null) {	
-					artModuleUl.append( "<li class='artMetaData'>" + "<h3><a target='_blank' href=" + artLink + ">" + artTitle + "</a></h3></li>"); // title & link to item
+					artModuleUl.append( "<li class='artMetaData'>" + "<h3><a target='_blank' href=" + artLink + ">" + "<span class='title'>"+ artTitle + "</span></a></h3></li>"); // title & link to item
 					// artModuleUl.append("<li class='artMetaData'><span class='fieldType'>Title: </span>" + artTitle +"</li>");
-					artModuleUl.append("<li class='artMetaData'><span class='fieldType'>Creator: </span>" + artMaker +"</li>");
+					artModuleUl.append("<li class='artMetaData'><span class='fieldType'>Creator: </span><span class='maker'>" + artMaker +"</span></li>");
 				}
 
 				// let's inject the location (only if it doesn't return as undefined) into the page
