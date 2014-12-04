a78cd299c2a881dac1615e742eb7586cebb077c2
“Melissa” <melissa@melissajclark.ca>
2014-12-04 15:41:16 -0500

    upped the returned items to 200 - testing

--- museumsAPI.js
+++ museumsAPI.js
@@ -44,7 +44,7 @@ artApp.getPieces = function(query) { // create a method to go and grab the artwo
 		data: {		
 			key: artApp.RMkey,
 			format: "jsonp",
-			ps: 20,
+			ps: 200,
 			imgonly: true,
 			culture: "en",
 			q: query, 
