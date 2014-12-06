fefac9ccae4a421592faec007f9ff4cb62535bd5
Melissa Jean Clark <melissa@melissajclark.ca>
2014-12-05 20:24:51 -0500

    removed notvisible class from ul removed function attempt trying readmore.js

--- museumsAPI.js
+++ museumsAPI.js
@@ -59,7 +59,7 @@ artApp.getPieces = function(query) { // create a method to go and grab the artwo
 
 artApp.displayPieces = function(pieces) {
 
-	var artModuleTmpl = $("<section class='artworkModule'><section class='artImage'></section><ul class='artFields notVisible'></ul></section>");
+	var artModuleTmpl = $("<section class='artworkModule'><section class='artImage'></section><ul class='artFields'></ul></section>");
 	// loop over each piece
 	console.log(pieces.length); // counts number of pieces retreived by API
 	for (var i = 0; i < pieces.length; i++) { // for loop
@@ -162,10 +162,8 @@ artApp.displayPieces = function(pieces) {
 			}
 
 			$("#artwork").append(artModuleSection);
-				// $("ul.artFields").addClass(notVisible);
-				$(".readMore").on("click", function(){
-					$("ul.artFields").removeClass(notVisible);
-				});
+
+			$("ul.artFields").readmore();
 			} // end success function
 		}); // end ajax function
 
