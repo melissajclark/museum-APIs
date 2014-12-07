44098bcb2a5e1d0c9d79db16eb56394d7192ded8
Melissa Jean Clark <melissa@melissajclark.ca>
2014-12-07 14:01:54 -0500

    removed extra notes and moved to new file

--- museumsAPI.js
+++ museumsAPI.js
@@ -189,39 +189,3 @@ $(document).ready(function(){
 	// calls ALL of the code above! 
 
 });
-
-/*========================================
-=            Extra Code Stuff            =
-========================================
-
-// below: variables - commented out because they are not currently in use (storing for later)
-
-// var artMakerBorn = artPiece.principalMakers[0].dateOfBirth;
-// var artMakerDied = artPiece.principalMakers[0].dateOfDeath;
-// var artMakerVal = artPiece.principalOrFirstMaker.val();
-// var artDescription = artPiece.description;
-//  var artColours = artPiece.colors;
-
-// if (artPiece.webImage !== null && artColours !== undefined && artColours !== null && artColours !== Array[0]){	// injects the location only if it exists
-// 	artModuleUl.append("<li class='artMetaData'><span class='fieldType'>Colours: </span><span class='colours' data-colours='" + artColours + "style=background-color:" + artColours + ";'" + artColours + "</span></li>");
-// }
-
-art colours variable = appends list of hexcodes - need to split them all to play with colours
-
-// var artInscriptions = artPiece.inscriptions;
-// var artVIP = artPiece.historicalPersons;
-// var artLabel = artPiece.label;
-
-
-// console.log("Inscription: " + artInscriptions);
-// console.log("VIPs: " + artVIP);
-// console.log("Label: " + artLabel);
-
-
-// console.log("Physical Medium: " + artMedium)
-// console.log("Type: " + artType);
-// console.log("Materials: " + artMaterials);
-// console.log("Technique: " + artTechnique);
-
-
------  End of Extra Code Stuff  ------*/
