a67e0d27a76b5116dd2cc8d707379f2a02658d99
“Melissa” <melissa@melissajclark.ca>
2014-12-14 18:04:44 -0500

    added else statement for 3rd century

--- museumsAPI.js
+++ museumsAPI.js
@@ -154,6 +154,9 @@ artApp.displayPieces = function(pieces) {
 			if (artPeriodLastDigit === "4" || artPeriodLastDigit === "5" || artPeriodLastDigit === "6" || artPeriodLastDigit === "7" || artPeriodLastDigit === "8" || artPeriodLastDigit === "9") {
 				artPeriod = artPeriodString + "th Century";
 				console.log(artPeriod);
+			} else if (artPeriodLastDigit === "3" && artPeriod != 13 ) {
+				artPeriod = artPeriodString + "rd Century"; // for the 3rd centrury
+
 			}
 
 
